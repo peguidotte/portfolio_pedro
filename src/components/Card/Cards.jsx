c847c9ab09aaead48725d8729bfd78aa479cfe97
Pedro Visconti Guidotte <peguidotte@gmail.com>
2024-10-04 19:46:49 -0300

    Projects meramente estilizados

--- src/components/Card/Cards.jsx
+++ src/components/Card/Cards.jsx
@@ -1,12 +1,23 @@
 import PropTypes from "prop-types";
 
-function Cards({ name, description }) {
+function Cards({ name, description, image, repo, deploy, date, tools }) {
   return (
-    <li className="flex flex-col items-center">
-      <span className="mt-2 text-[8px] sm:text-base font-principal">
+    <li className="flex flex-col items-center p-2 border border-orange-500 rounded-2xl">
+      <img src={image} alt={name} className="rounded-2xl"/>
+      <h1 className="mt-2 text-[8px] sm:text-base font-principal">
         {name}
-      </span>
+      </h1>
       <p>{description}</p>
+      <a href={repo}>Repo</a>
+      <a href={deploy}>Deploy</a>
+      <span>{date}</span>
+      {tools.map((tool, index) => {
+        if (tool.icon) {
+          const Icon = tool.icon;
+          return <Icon key={index} style={{ color: tool.color }} />;
+        }
+        return null;
+      })}
     </li>
   );
 }
@@ -14,6 +25,16 @@ function Cards({ name, description }) {
 Cards.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  repo: PropTypes.string.isRequired,
+  deploy: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  tools: PropTypes.arrayOf(
+    PropTypes.shape({
+      icon: PropTypes.elementType.isRequired,
+      color: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Cards;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,90 +6,118 @@ import {
   RiReactjsFill,
   RiTailwindCssFill,
 } from "react-icons/ri";
+import EpitStopImage from "../assets/EpitStop.png";
+import MelodiaImage from "../assets/Melodia.png";
+import EcoTrendImage from "../assets/EcoTrend.png";
+import WasteWiseImage from "../assets/WasteWise.png";
+import MiniRedeSocialImage from "../assets/MiniRedeSocial.png";
+import PortfolioImage from "../assets/Portfolio.png";
+
+
 import { FaSass } from "react-icons/fa";
-import Cards from "../components/Card/Cards.jsx"
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import Cards from "../components/Card/Cards.jsx";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
 
 function Projects() {
   const projectsData = [
     {
       name: "E-PitStop",
       description: "Projeto Fórmula E",
-      image: "../assets/EpitStop.png",
+      image: EpitStopImage,
       repo: "https://github.com/peguidotte/E-PitStop__React",
       deploy: "https://e-pit-stop.vercel.app/",
       date: "present",
-      tools: [{ RiReactjsFill }, { RiTailwindCssFill }],
+      tools: [
+        { icon: RiReactjsFill, color: "#61DAFB" },
+        { icon: RiTailwindCssFill, color: "#38B2AC" },
+      ],
     },
     {
       name: "Melodia",
       description: "Projeto Melodia",
-      image: "../assets/Melodia.png",
+      image: MelodiaImage,
       repo: "https://github.com/peguidotte/Melodia",
       deploy: "https://melodia-bay.vercel.app/",
       date: "10/2024",
-      tools: [{ RiBootstrapFill }, { FaSass }, { RiHtml5Fill }],
+      tools: [
+        { icon: RiBootstrapFill, color: "#7952B3" },
+        { icon: FaSass, color: "#CC6699" },
+        { icon: RiHtml5Fill, color: "#E34F26" },
+      ],
     },
     {
       name: "EcoTrend",
       description: "Projeto EcoTrend",
-      image: "../assets/EcoTrend.png",
+      image: EcoTrendImage,
       repo: "https://github.com/peguidotte/EcoTrend",
       deploy: "https://eco-trend-lac.vercel.app/",
       date: "09/2024",
-      tools: [{ RiBootstrapFill }, { RiCss3Fill }, { RiHtml5Fill }],
+      tools: [
+        { icon: RiBootstrapFill, color: "#7952B3" },
+        { RiCss3Fill, color: "#1572B6" },
+        { icon: RiHtml5Fill, color: "#E34F26" },
+      ],
     },
     {
       name: "WasteWise",
       description: "Projeto WasteWise",
-      image: "../assets/WasteWise.png",
+      image: WasteWiseImage,
       repo: "https://github.com/peguidotte/WasteWise",
       deploy: "https://waste-wise-ibmn.vercel.app/",
       date: "06/2024",
-      tools: [{ RiJavascriptFill }, { RiCss3Fill }, { RiHtml5Fill }],
+      tools: [
+        { icon: RiJavascriptFill, color: "#F7DF1E" },
+        { icon: RiCss3Fill, color: "#1572B6" },
+        { icon: RiHtml5Fill, color: "#E34F26" },
+      ],
     },
     {
       name: "Mini Rede-Social",
       description: "Projeto Mini Rede-Social",
-      image: "../assets/MiniRedeSocial.png",
+      image: MiniRedeSocialImage,
       repo: "https://github.com/peguidotte/Mini-Rede-Social",
       deploy: "https://peguidotte.github.io/Mini-Rede-Social/",
       data: "04/2024",
-      tools: [{ RiJavascriptFill }, { RiCss3Fill }, { RiHtml5Fill }],
+      tools: [
+        { icon: RiJavascriptFill, color: "#F7DF1E" },
+        { icon: RiCss3Fill, color: "#1572B6" },
+        { icon: RiHtml5Fill, color: "#E34F26" },
+      ],
     },
     {
       name: "Portfólio",
       description: "Projeto Portfólio",
-      image: "../assets/Portfólio.png",
+      image: PortfolioImage,
       repo: "https://github.com/peguidotte/portfolio_pedro",
       deploy: "",
       data: "10/2024",
-      tools: [{ RiReactjsFill }, { RiTailwindCssFill }],
+      tools: [
+        { icon: RiReactjsFill, color: "#61DAFB" },
+        { icon: RiTailwindCssFill, color: "#38B2AC" },
+      ],
     },
   ];
 
-  const projectCards = projectsData.map((skill, index) => (
-    <Cards key={index} description={skill.description} name={skill.name}/>
-));
+  const projectCards = projectsData.map((project, index) => (
+    <Cards
+      key={index}
+      name={project.name}
+      description={project.description}
+      image={project.image}
+      repo={project.repo}
+      deploy={project.deploy}
+      date={project.date}
+      tools={project.tools}
+    />
+  ));
 
   return (
-    <ul className="mt-5 sm:mt-10 mb-10 flex justify-center space-x-4">
-                <AliceCarousel
-                    items={projectCards}
-                    autoPlay
-                    autoPlayInterval={1000}
-                    infinite
-                    disableButtonsControls
-                    disableDotsControls
-                    responsive={{
-                        0: { items: 5 },
-                        600: { items: 6 },
-                        1100: { items: 9 },
-                    }}
-                />
-            </ul>
-  )
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 p-8">
+      {projectCards}
+    </ul>
+  );
 }
 
 export default Projects;
