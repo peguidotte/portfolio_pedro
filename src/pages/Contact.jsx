568ff6e1ae073b18cd29d3a678005280c4e8399f
PedroGuidotte <peguidotte@gmail.com>
2024-10-06 12:43:30 -0300

    projectDetail finalizado, contact finalizado

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,90 @@
+import React, { useState } from 'react';
+
 function Contact() {
-    return (
-        <h1>Contact</h1>
-    )
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Nome:', name);
+    console.log('Email:', email);
+    console.log('Mensagem:', message);
+
+    // Exibir mensagem de sucesso
+    setSuccessMessage('Enviado com sucesso!');
+
+    // Reiniciar o formulário
+    setName('');
+    setEmail('');
+    setMessage('');
+
+    // Ocultar a mensagem de sucesso após 3 segundos
+    setTimeout(() => {
+      setSuccessMessage('');
+    }, 3000);
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-full max-w-lg">
+        <h1 className="text-2xl font-bold mb-4">Contato</h1>
+        {successMessage && (
+          <div className="mb-4 text-green-500 font-bold">
+            {successMessage}
+          </div>
+        )}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Nome
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+            Mensagem
+          </label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            rows="5"
+            required
+          ></textarea>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-orange-500 via-red-600 to-pink-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
--- src/pages/ProjectsDetail.jsx
+++ src/pages/ProjectsDetail.jsx
@@ -1,55 +1,91 @@
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import projectsData from '../projectsData.json';
 import {
-  RiBootstrapFill,
-  RiCss3Fill,
-  RiHtml5Fill,
-  RiJavascriptFill,
-  RiReactjsFill,
-  RiTailwindCssFill,
+    RiBootstrapFill,
+    RiCss3Fill,
+    RiGithubFill,
+    RiHtml5Fill,
+    RiJavascriptFill,
+    RiPagesFill,
+    RiReactjsFill,
+    RiTailwindCssFill,
 } from "react-icons/ri";
 import { FaSass } from "react-icons/fa";
+import { TiArrowBack } from "react-icons/ti";
+import '../components/Card/Cards.css'
 
 const iconMap = {
-  RiBootstrapFill,
-  RiCss3Fill,
-  RiHtml5Fill,
-  RiJavascriptFill,
-  RiReactjsFill,
-  RiTailwindCssFill,
-  FaSass
+    RiBootstrapFill,
+    RiCss3Fill,
+    RiHtml5Fill,
+    RiJavascriptFill,
+    RiReactjsFill,
+    RiTailwindCssFill,
+    FaSass
 };
 
+//"bg-gradient-to-r from-orange-400 via-red-500 to-pink-600 text-white py-2 px-4 rounded-3xl"
+
 function ProjectDetail() {
-  const { name } = useParams();
-  const project = projectsData.find(p => p.name === name);
+    const { name } = useParams();
+    const project = projectsData.find(p => p.name === name);
 
-  if (!project) {
-    return <div>Project não encontrado 😞</div>;
-  }
+    if (!project) {
+        return <div>Project não encontrado 😞</div>;
+    }
 
-  return (
-    <div>
-      <h1>Detalhes: {project.name}</h1>
-      <img src={project.image} alt={project.name} />
-      <p>{project.details}</p>
-      <p>{project.target}</p>
-      <p>{project.style}</p>
-      <div>
-        <h2>Ferramentas Utilizadas:</h2>
-        <ul>
-          {project.tools.map((tool, index) => {
-            const IconComponent = iconMap[tool.icon];
-            return (
-              <li key={index} style={{ color: tool.color }}>
-                <IconComponent />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className='font-principal mt-4 flex flex-col gap-6 sm:gap-10 sm:p-2 md:p-5 lg:p-10'>
+            <NavLink
+                to="/projects"
+                className="font-semibold flex items-center gap-2 text-xl"
+            >
+                <TiArrowBack /> Voltar
+            </NavLink>
+            <h1 className='bg-gradient-to-r from-orange-400 via-red-500 to-pink-600 bg-clip-text text-transparent text-2xl font-bold sm:text-5xl md:text-center md:self-center'>
+                {project.name}
+            </h1>
+            <img src={project.image} alt={project.name} className='shadow-box rounded-2xl md:w-5/6 xl:w-4/6 text-center self-center' />
+            <h2 className='bg-gradient-to-r from-orange-400 via-red-500 to-pink-600 bg-clip-text text-transparent text-lg font-medium
+                sm:text-2xl
+                '>
+                Detalhes:
+            </h2>
+            <div className='flex flex-col text-sm gap-6 text-pretty text-justify p-1 sm:text-lg'>
+                <p>{project.details}</p>
+                <p>{project.target}</p>
+                <p>{project.style}</p>
+            </div>
+            <div className='flex items-center justify-between sm:justify-start sm:gap-10'>
+                <h2 className='bg-gradient-to-r from-orange-400 via-red-500 to-pink-600 bg-clip-text text-transparent text-lg font-medium
+                    sm:text-2xl
+                    '>
+                    Tecnologias:
+                </h2>
+                <ul className='flex gap-6 text-5xl '>
+                    {project.tools.map((tool, index) => {
+                        const IconComponent = iconMap[tool.icon];
+                        return (
+                            <li key={index} style={{ color: tool.color }}>
+                                <IconComponent />
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className='flex justify-around items-center'>
+                <a href={project.repo} target='_blank' className='flex items-center gap-2 text-xl'><RiGithubFill /> Repositório</a>
+                <a href={project.deploy} target='_blank' className='flex items-center gap-2 text-xl'><RiPagesFill />Deploy</a>
+            </div>
+            <h4 className='text-gray-700'>Conclusão: {project.date}</h4>
+            <NavLink
+                to="/projects"
+                className="font-semibold flex items-center gap-2 text-xl"
+            >
+                <TiArrowBack /> Voltar
+            </NavLink>
+        </div>
+    );
 }
 
 export default ProjectDetail;
